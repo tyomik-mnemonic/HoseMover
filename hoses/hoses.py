043e33873bb92a9b3b8a828971418e74db9bf852
toemik_rescuer <vins90nuar@gmail.com>
2023-11-20 21:38:41 +0300

    temp changes

---
+++ hoses/hoses.py
@@ -0,0 +1,27 @@
+from hoses.abc_hose import Hose
+
+
+class FiftyOneHose(Hose):
+    
+    def __init__(
+        self,
+        
+    ):  
+        super().__init__()
+        self.diameter = 51
+        self.pressure = 16
+        self.resistance = 0.13
+        self.throughput_rate = 11
+        self.weight = 0.45
+        self.status = None
+        self.discription = "OK"
+        
+class HoseFabric:
+    
+    @staticmethod
+    def get_hose(diameter:str):
+        
+        if diameter == 51:
+            return FiftyOneHose()
+        elif diameter != 51:
+            return None
---
+++ main.py
@@ -0,0 +1,3 @@
+from hoses.hoses import HoseFabric as fabric
+
+fifty_one_first = fabric.get_hose(51)
